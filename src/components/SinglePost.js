021029637d1712dc9fb7385ae3f96d77012624ea
tanuj7077 <piku.tanuj@gmail.com>
2021-03-30 15:43:41 +0530

    hoverInfo complete

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,38 +1,67 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
 
 const SinglePost = ({ _id, image, name, author, likesArray, comments }) => {
-  // let postUrl = "http://localhost:8000/posts/post/" + _id;
-  // const [post, setPost] = useState(null);
-  // async function fetchPost() {
-  //   try {
-  //     const response = await fetch(postUrl);
-  //     await response.json().then((data) => {
-  //       console.log(data);
-  //       setPost(data);
-  //     });
-  //     //setComment(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-  // useEffect(() => {
-  //   fetchPost();
-  // }, [_id]);
+  let userUrl = "http://localhost:8000/users/hoverUser/" + author.id;
+
   const AuthorHoverRef = useRef(null);
   const [isHovered, setIsHovered] = useState(false);
+  const [username, setUsername] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [postCount, setPostCount] = useState(0);
+  const [followerCount, setFollowerCount] = useState(0);
+  const [coverPhoto, setCoverPhoto] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+
   const handleMouseEnter = () => {
+    //clear all data from previous hover
+    if (username !== author.username) {
+      setUsername("");
+      setPosts([]);
+      setPostCount(0);
+      setFollowerCount(0);
+      setCoverPhoto("");
+      setProfilePic("");
+    }
     setIsHovered(true);
     setTimeout(() => {
       if (AuthorHoverRef.current) {
         document.getElementById("authorHover").classList.add("show");
+        if (!username) {
+          document.getElementById("authorHover").classList.add("loading");
+        } else {
+          document.getElementById("authorHover").classList.remove("loading");
+        }
         positionItem();
       }
-    }, 500);
+      if (username !== author.username) {
+        //for not fetching data again and again
+        //if same user is hovered
+        getHoverData();
+      }
+    }, 100);
   };
+
+  const getHoverData = async () => {
+    await axios.get(userUrl).then((res) => {
+      console.log(res.data);
+      if (AuthorHoverRef.current) {
+        document.getElementById("authorHover").classList.remove("loading");
+      }
+      setUsername(res.data.username);
+      setPosts(res.data.posts);
+      setPostCount(res.data.postCount);
+      setFollowerCount(res.data.followerCount);
+      setCoverPhoto(res.data.coverImage);
+      setProfilePic(res.data.profilePic);
+    });
+  };
+
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+
   const positionItem = () => {
     let top = AuthorHoverRef.current.getBoundingClientRect().top;
     let left = AuthorHoverRef.current.getBoundingClientRect().left;
@@ -47,12 +76,9 @@ const SinglePost = ({ _id, image, name, author, likesArray, comments }) => {
         -(right - window.innerWidth) + "px";
     }
     if (bottom > window.innerHeight) {
-      console.log("bottom");
       document.getElementById("authorHover").style.top =
         -(bottom - window.innerHeight + 110) + "px";
     }
-
-    console.log(top, left, bottom, right);
   };
 
   return (
@@ -71,7 +97,47 @@ const SinglePost = ({ _id, image, name, author, likesArray, comments }) => {
               onMouseLeave={handleMouseLeave}
               ref={AuthorHoverRef}
               id="authorHover"
-            ></div>
+            >
+              {!username && (
+                <>
+                  <div className="authorHover-loading--cover"></div>
+                  <div className="authorHover-loading--profile"></div>
+                  <div className="authorHover-loading--name"></div>
+                  <div className="authorHover-loading--info"></div>
+                  <div className="authorHover-loading--images"></div>
+                </>
+              )}
+              {username && (
+                <>
+                  <div
+                    className="authorHover--cover"
+                    style={{
+                      backgroundImage: `linear-gradient(to bottom,transparent,rgba(43, 43, 43, 0.9)), url(${coverPhoto})`,
+                    }}
+                  ></div>
+                  <div className="authorHover--profile"></div>
+                  <div className="authorHover--name">{username}</div>
+                  <div className="authorHover--info">
+                    <span className="info">{postCount} Posts</span>
+                    <span className="lineBreak">|</span>
+                    <span className="info">{followerCount} Followers</span>
+                  </div>
+                  <div className="authorHover--images">
+                    {/* if images count > 3 add "SEE MORE"*/}
+                    {posts.map((post) => {
+                      return (
+                        <div
+                          className="image"
+                          style={{
+                            backgroundImage: `url(${post.image})`,
+                          }}
+                        ></div>
+                      );
+                    })}
+                  </div>
+                </>
+              )}
+            </div>
           )}
           {/* <div
             className={`${isHovered ? `authorHover show` : `authorHover`}`}
