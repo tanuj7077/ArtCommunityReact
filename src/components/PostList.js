4cff7a0ec359b833d63c2fd7b68f7e8b399d2e6c
tanuj7077 <piku.tanuj@gmail.com>
2021-04-05 12:40:34 +0530

    post pagination homepage

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import axios from "axios";
 
@@ -9,29 +9,55 @@ const url = "http://localhost:8000/posts";
 const PostList = () => {
   const { posts, setPosts } = useGlobalContext();
 
-  const fetchPosts = async () => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        setPosts(result);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(12);
+  const [loading, setLoading] = useState(false);
 
+  const fetchPosts = async () => {
+    setLoading(true);
+    let baseUrl = "http://localhost:8000/posts/p";
+    const urlPage = `?page=${page}`;
+    const urlLimit = `&limit=${limit}`;
+    let url = `${baseUrl}${urlPage}${urlLimit}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPosts((oldPhotos) => {
+        return [...oldPhotos, ...data];
+      });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        (!loading && window.innerHeight + window.scrollY) >=
+        document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //to update posts array in the backend
   const updatePostsBackend = async () => {
     const result = await axios.get("http://localhost:8000/posts/updatePosts");
-    const pagination = await axios.get(
-      "http://localhost:8000/posts/p?page=1&limit=5"
-    );
     console.log(result.data);
-    console.log(pagination.data);
   };
 
   useEffect(() => {
-    fetchPosts();
     updatePostsBackend();
   }, []);
 
@@ -47,6 +73,7 @@ const PostList = () => {
           })}
         </Masonry>
       </ResponsiveMasonry>
+      {loading && <span className="loading">Loading...</span>}
     </div>
   );
 };
