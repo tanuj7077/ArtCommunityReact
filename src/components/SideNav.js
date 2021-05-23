38d05cbdb15cdf9296ff0630a88ffd7a1ba8989e
tanuj7077 <piku.tanuj@gmail.com>
2021-05-23 15:44:49 +0530

    sideNav functionality code reduction

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,95 +1,101 @@
 import React, { useState, useEffect } from "react";
-import { Link, Route } from "react-router-dom";
-import { LoggedInSideNavHover, SideNavHover } from "../externalJs/sideNavUtil";
+import { Route } from "react-router-dom";
 import { LoggedInSideNavItems, SideNavItems } from "../data";
 import { useGlobalContext } from "../context";
 
 const SideNav = () => {
   const { isLoggedIn } = useGlobalContext();
-  async function checkLoggedIn() {
+  const [items, setItems] = useState();
+  const checkLoggedIn = () => {
     if (isLoggedIn) {
-      LoggedInSideNavHover();
+      setItems(LoggedInSideNavItems);
     } else {
-      SideNavHover();
+      setItems(SideNavItems);
     }
-  }
+  };
+
+  const addHover = (item) => {
+    console.log(item);
+    document
+      .getElementsByClassName("sideNav-expansion--" + item)[0]
+      .classList.add("sideNavTitle-hover");
+    document
+      .getElementsByClassName("sideNav--" + item)[0]
+      .classList.add("sideNavIcon-hover");
+  };
+  const removeHover = (item) => {
+    document
+      .getElementsByClassName("sideNav-expansion--" + item)[0]
+      .classList.remove("sideNavTitle-hover");
+    document
+      .getElementsByClassName("sideNav--" + item)[0]
+      .classList.remove("sideNavIcon-hover");
+  };
+
+  const sideNavToggleHandler = () => {
+    document
+      .getElementsByClassName("sideNav-expansion")[0]
+      .classList.toggle("expanded");
+  };
 
   useEffect(() => {
     checkLoggedIn();
-    //--------expansion--------
-    const expansion = document
-      .getElementsByClassName("sideNav--ham")[0]
-      .addEventListener("click", () => {
-        document
-          .getElementsByClassName("sideNav-expansion")[0]
-          .classList.toggle("expanded");
-      });
-
-    /*return () =>
-      document
-        .getElementsByClassName("sideNav--ham")[0]
-        .removeEventListener("click", expansion);
-*/
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  //setTimeout(checkLoggedIn, 3000);
-
-  let items;
-  if (isLoggedIn) {
-    items = LoggedInSideNavItems;
-  } else {
-    items = SideNavItems;
-  }
+  }, [isLoggedIn]);
 
   return (
     <>
       <section className="sideNav">
-        <div className="sideNav--ham">
+        <div className="sideNav--ham" onClick={sideNavToggleHandler}>
           <span className="material-icons sideNav--icon">menu</span>
         </div>
-        {items.map((item) => {
-          return (
-            <Route
-              key={item.id}
-              render={({ history }) => (
-                <div
-                  onClick={() => {
-                    history.push(`/${item.link}`);
-                  }}
-                  key={item.id}
-                  className={`sideNav--${item.classname}`}
-                >
-                  <span className="material-icons">{item.icon}</span>
-                </div>
-              )}
-            />
-          );
-        })}
+        {items &&
+          items.map((item) => {
+            return (
+              <Route
+                key={item.id}
+                render={({ history }) => (
+                  <div
+                    onClick={() => {
+                      history.push(`/${item.link}`);
+                    }}
+                    key={item.id}
+                    className={`sideNav--${item.classname}`}
+                    onMouseEnter={() => addHover(item.classname)}
+                    onMouseLeave={() => removeHover(item.classname)}
+                  >
+                    <span className="material-icons">{item.icon}</span>
+                  </div>
+                )}
+              />
+            );
+          })}
       </section>
       <section className="sideNav-expansion">
-        {items.map((item) => {
-          return (
-            <Route
-              key={item.id2}
-              render={({ history }) => (
-                <div
-                  onClick={() => {
-                    history.push(`/${item.link}`);
-                  }}
-                  key={item.id2}
-                  className={`sideNav-expansion--${item.classname}`}
-                >
-                  <span
-                    className={`sideNav-expansion--${item.classname}-title`}
+        {items &&
+          items.map((item) => {
+            return (
+              <Route
+                key={item.id2}
+                render={({ history }) => (
+                  <div
+                    onClick={() => {
+                      history.push(`/${item.link}`);
+                    }}
+                    key={item.id2}
+                    className={`sideNav-expansion--${item.classname}`}
+                    onMouseEnter={() => addHover(item.classname)}
+                    onMouseLeave={() => removeHover(item.classname)}
                   >
-                    {item.name}
-                  </span>
-                </div>
-              )}
-            />
-          );
-        })}
+                    <span
+                      className={`sideNav-expansion--${item.classname}-title`}
+                    >
+                      {item.name}
+                    </span>
+                  </div>
+                )}
+              />
+            );
+          })}
       </section>
     </>
   );
