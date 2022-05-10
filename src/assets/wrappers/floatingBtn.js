172b87d8526821ea60f70a3086c88fda1418cac7
Tanuj <piku.tanuj@gmail.com>
2022-05-10 18:24:42 +0530

    floatingBtn icon positioning

--- src/assets/wrappers/floatingBtn.js
+++ src/assets/wrappers/floatingBtn.js
@@ -8,16 +8,20 @@ export const Wrapper = styled.button`
   outline: none;
   background-color: #20bf6b;
   border-radius: 50%;
-  display: grid;
+  /* display: grid;
+  place-items: center; */
   width: 4rem;
   height: 4rem;
-  place-items: center;
   color: white;
   display: none;
   @media only screen and (max-width: 37.5em) {
     display: block;
   }
   .icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     width: 2.5rem;
     height: 2.5rem;
   }
